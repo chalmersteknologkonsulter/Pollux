d1e27e8f6e5399a2935658d9ef70c82dc84d87d9
saquand.elia@aol.fr <saquand.elia@aol.fr>
2020-05-10 22:34:16 +0200

    choice

--- express_installation.py
+++ express_installation.py
@@ -1,49 +1,44 @@
 import os
 import platform
 
-#install the application generator as a global npm package 
-cmd1 = "sudo npm install -g express-generator"
-print(cmd1)
-run_cmd1 = os.system(cmd1)
+install_express = input("Would like to build an express server? Yes/No ")
 
-#launch the application generator
-#os.system("express")
-#print(run_cmd1)
-#trial2 = os.system("express -h")
-#print(trial2)
+if (install_express == "No"):
+  exit
+else:
+  #install the application generator as a global npm package 
+  # attention faut avoir npm installed on your computer
+  cmd1 = "sudo npm install -g express-generator"
+  print(cmd1)
+  run_cmd1 = os.system(cmd1)
 
-app_name = input("What is the name of your app?")
-#print(app_name)
+  #launch the application generator
+  #os.system("express")
+  #trial2 = os.system("express -h")
 
-cmd2 = "express --view=pug " + app_name
-print(cmd2)
-run_cmd2 = os.system(cmd2)
-#print(run_cmd2)
+  app_name = input("What is the name of your app?")
 
-run_cmd3 = os.chdir(app_name)
-# varify the path using getcwd() 
-cwd = os.getcwd() 
-# print the current directory 
-print("Current working directory is:", cwd)
+  cmd2 = "express --view=pug " + app_name
+  print(cmd2)
+  run_cmd2 = os.system(cmd2)
 
-cmd4 = "npm install"
-print(cmd4)
-run_cmd4 = os.system(cmd4)
-#print(run_cmd4)
+  run_cmd3 = os.chdir(app_name)
+  # varify the path using getcwd() 
+  cwd = os.getcwd() 
+  # print the current directory 
+  print("Current working directory is:", cwd)
 
-# os_type = input("""
-# Which os are you using? (Type the corresponding number)
-#1. Windows
-#2. MacOS/Linux
-#""")
-# pr windows faaire pr MacOs/linux
-os_type = platform.system()
-print(os_type)
-if (os_type == "Linux" or os_type == "Darwin"):
-  cmd5 = "DEBUG=" + app_name + ":* npm start"
-elif (os_type == "Windows"):
-  cmd5 = "SET DEBUG=" + app_name + ":* & npm start"
+  cmd4 = "npm install"
+  print(cmd4)
+  run_cmd4 = os.system(cmd4)
 
-print(cmd5)
-run_cmd5 = os.system(cmd5)
-#print(run_cmd5)
+  os_type = platform.system()
+  print(os_type)
+  if (os_type == "Linux" or os_type == "Darwin"):
+    cmd5 = "DEBUG=" + app_name + ":* npm start"
+  elif (os_type == "Windows"):
+    cmd5 = "SET DEBUG=" + app_name + ":* & npm start"
+
+  print(cmd5)
+  run_cmd5 = os.system(cmd5)
+  
