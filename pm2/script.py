ab06fbfcf7e9913d096d9160314da64dfb562e99
deriye <>
2020-06-23 15:16:09 +0200

    update

--- pm2/script.py
+++ pm2/script.py
@@ -5,5 +5,5 @@ os.system("sudo apt install nodejs")
 os.system("sudo apt install npm")
 os.system("sudo npm i -g pm2")
 
-file_path = raw_put("Please enter your app.js file path")
-os.system("sudo pm2 start" + file_path + "\\app.js")
+file_path = raw_put("Please enter your startup file path")
+os.system("sudo pm2 start " + file_path)
