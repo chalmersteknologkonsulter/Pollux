4301758c3e5f6da0736c19519d507cd54cbc848a
KardoMarof <gusmaroka@student.gu.se>
2020-06-26 14:19:18 +0200

    Optimize reac-native.py script

--- react-native/react-native.py
+++ react-native/react-native.py
@@ -2,18 +2,18 @@ import os
 import functions as f
 import platform
 
-install = raw_input("\nDo you want install React Native? (Y for Yes, N for No)\n")
+install = input("\nDo you want install React Native? y/n \n")
 
-if (install == "Y"):
-    print("\nChoose a CLI type and a project name to create your React Native project\n")
+if (install == "y"):
+    print("\n Choose a CLI type and a project name to create your React Native project\n")
 
-    cli_type = raw_input("CLI type (E for Expo, R for React): ")
+    cli_type = input("\n CLI type: \n 1. Expo \n 2. React \n ")
 
     # if CLI type input is incorrect
-    while ((cli_type != "E") and (cli_type != "R")):
-        cli_type = raw_input("CLI type incorrect! Please choose a valid CLI type (E for Expo, R for React): ")
+    while ((cli_type != "1") and (cli_type != "2")):
+        cli_type = input("CLI type incorrect! Please choose a valid CLI type. Insert 1 for Expo, or 2 for React. ")
 
-    project_name = raw_input("Poject name: ")
+    project_name = input("Poject name: ")
     print("")
 
     if (platform.system() == "Linux"):
@@ -30,14 +30,14 @@ if (install == "Y"):
         download_source = "https://developer.android.com/studio"
 
         if (platform.system() == "Darwin"):
-            dev_platform = raw_input("Do you want to React Native for iOS or Android? (I for iOS, A for Android): ")
-            if (dev_platform == "I"):
+            dev_platform = input("Do you want to React Native for iOS or Android? \n 1. iOS \n 2. Android \n  ")
+            if (dev_platform == "1"):
                 dev_platform_name = "Xcode"
                 download_source = "https://apps.apple.com/us/app/xcode/id497799835?mt=12"
-            
-        installed = raw_input("To use React CLI, you need to install " + dev_platform_name + ". \nIs it already installed (Y for yes, N for no)? ")
 
-        if (installed == "Y"):
+        asInstalled = input("To use React CLI, you need to install " + dev_platform_name + ". \nIs it already installed? y/n ")
+
+        if (asInstalled == "y"):
             f.react(project_name, dev_platform_name)
         else:
             print("\nYou can download from " + download_source + "\n")
