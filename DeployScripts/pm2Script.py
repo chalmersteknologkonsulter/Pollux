8bc3edb8560f1d3e6c406b932dcfccefc85e2d6b
KardoMarof <gusmaroka@student.gu.se>
2020-07-01 11:23:57 +0200

    Configure pm2 script, refactor/clean file structure

--- DeployScripts/pm2Script.py
+++ DeployScripts/pm2Script.py
@@ -5,5 +5,7 @@ os.system("sudo apt install nodejs")
 os.system("sudo apt install npm")
 os.system("sudo npm i -g pm2")
 
+appName = input("\nSpecify the name of your app: ")
 file_path = input("\nPlease enter the path to your startup file: ")
-os.system("sudo pm2 start " + file_path)
+os.system("sudo pm2 start " + file_path + " --name " + appName)
+os.system("pm2 status")
