6fcaa08ac7a00121225bf68745ae2fd972aaa57f
KardoMarof <gusmaroka@student.gu.se>
2020-07-06 15:28:10 +0200

    Remove sudo from pm2 command. (Dont run your application on sudo! Its bad security practice)

--- DeployScripts/pm2Script.py
+++ DeployScripts/pm2Script.py
@@ -7,5 +7,5 @@ os.system("sudo npm i -g pm2")
 
 appName = input("\nSpecify the name of your app: ")
 file_path = input("\nPlease enter the path to your startup file: ")
-os.system("sudo pm2 start " + file_path + " --name " + appName)
+os.system("pm2 start " + file_path + " --name " + appName)
 os.system("pm2 status")
